73acf60e95134ca863f8ebafffe1bb1f28f6cc51
Ha Junyoung <jun19973@naver.com>
2026-04-16 15:29:57 +0900

    Initial Commit: 프로젝트 초기 설정

---
+++ src/main/java/com/sparta/delivhub/DelivhubApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.delivhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/delivhub/DelivhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.delivhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
