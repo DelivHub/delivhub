4c59633e14d13241c200184da616dd7d66c82850
jaehoon0321 <jaehoon0321@gmail.com>
2026-04-20 12:03:24 +0900

    feat: pr템플릿 추가 됐는지 확인

--- src/main/java/com/sparta/delivhub/order/controller/OrderController.java
+++ src/main/java/com/sparta/delivhub/order/controller/OrderController.java
@@ -3,5 +3,6 @@ package com.sparta.delivhub.order.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class OrderController {
+public class OrderController {//PR테스틀 위한 수정 주석 및 추가
+
 }
