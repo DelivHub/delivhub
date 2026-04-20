0994252fdaeda382e22aab8c68bdb5d2aa432b22
Ha Junyoung <jun19973@naver.com>
2026-04-20 15:20:21 +0900

    add common entity

--- src/main/java/com/sparta/delivhub/review/entity/Review.java
+++ src/main/java/com/sparta/delivhub/review/entity/Review.java
@@ -1,11 +1,27 @@
 package com.sparta.delivhub.review.entity;
 
-import jakarta.persistence.Entity;
+import com.sparta.delivhub.common.entity.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+import java.util.UUID;
+
 @Entity
+@Table(name = "p_reviews") // 명세서 도메인에 맞춰 테이블명 지정
+@Getter
 @NoArgsConstructor
-public class Review {
+public class Review extends BaseEntity { // 우리가 만든 BaseEntity 상속!
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // 명세서 요구사항인 UUID 적용
+    private UUID id;
+
+    private Integer rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private UUID orderId;
+    private UUID storeId;
 }
