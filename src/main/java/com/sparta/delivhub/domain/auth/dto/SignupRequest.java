6f3538c6417229d8a46b0842b0218541df3d274e
LeeSG-0114 <leesin0114@naver.com>
2026-04-22 00:20:29 +0900

    feat: 회원가입 요청,응답 dto 생성

---
+++ src/main/java/com/sparta/delivhub/domain/auth/dto/SignupRequest.java
@@ -0,0 +1,36 @@
+package com.sparta.delivhub.domain.auth.dto;
+
+import com.sparta.delivhub.domain.user.entity.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    @NotBlank(message = "아이디는 필수입니다.")
+    @Pattern(
+            regexp = "^[a-z0-9]{4,10}$",
+            message = "아이디는 4-10자의 소문자 영어 또는 숫자만 가능합니다."
+    )
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "비밀번호는 8-15자이며 영문, 숫자, 특수문자를 각각 최소 1개 이상 포함해야 합니다."
+    )
+    private String password;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "유효한 이메일 형식이 아닙니다."
+    )
+    private String email;
+
+    private UserRole role = UserRole.CUSTOMER;
+}
