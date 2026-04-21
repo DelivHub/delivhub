35f31be6727cec024341961ef056dcc66a7f6763
hyeeuncho <heeuncho@naver.com>
2026-04-21 14:14:56 +0900

    feat: Menu 엔티티 및 레포지토리 구현

---
+++ src/main/java/com/sparta/delivhub/domain/menu/controller/MenuController.java
@@ -0,0 +1,7 @@
+package com.sparta.delivhub.domain.menu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MenuController {
+}
---
+++ src/main/java/com/sparta/delivhub/domain/menu/service/MenuService.java
@@ -0,0 +1,7 @@
+package com.sparta.delivhub.domain.menu.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+}
