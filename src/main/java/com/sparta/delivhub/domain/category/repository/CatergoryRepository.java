15b2b0ee44739bafc38e67c8b0eab1974163657f
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2026-04-21 12:55:56 +0900

    feat: [KMW] area, category, store 엔티티, 레포지토리, 구현

---
+++ src/main/java/com/sparta/delivhub/domain/category/repository/CatergoryRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.delivhub.domain.category.repository;
+
+import com.sparta.delivhub.domain.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CatergoryRepository extends JpaRepository<Category, UUID> {
+}
