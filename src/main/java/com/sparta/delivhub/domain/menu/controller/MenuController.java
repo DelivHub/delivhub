ef98abd638d399c4d84b19a0f3f6a9a74f32b591
hyeeuncho <heeuncho@naver.com>
2026-04-21 14:31:16 +0900

    refactor: restcontroller로 수정'

--- src/main/java/com/sparta/delivhub/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/delivhub/domain/menu/controller/MenuController.java
@@ -1,7 +1,7 @@
 package com.sparta.delivhub.domain.menu.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MenuController {
 }
