d5c8cd5e0e2f9aad31f22c26eb8eb5e0fd62fe10
LeeSG-0114 <leesin0114@naver.com>
2026-04-21 22:23:40 +0900

    feat: UserRepository 내 유효 유저 조회 쿼리 메서드 정의

--- src/main/java/com/sparta/delivhub/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/delivhub/domain/user/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.sparta.delivhub.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsernameAndDeletedAtIsNull(String username);
 }
