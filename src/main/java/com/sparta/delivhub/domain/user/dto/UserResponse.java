77f3070cd93c032d839fff2546ccacddcb187e2a
LeeSG-0114 <leesin0114@naver.com>
2026-04-24 03:31:14 +0900

    feat: 유저 응답 dto 생성

---
+++ src/main/java/com/sparta/delivhub/domain/user/dto/UserResponse.java
@@ -0,0 +1,33 @@
+package com.sparta.delivhub.domain.user.dto;
+
+import com.sparta.delivhub.domain.user.entity.User;
+import com.sparta.delivhub.domain.user.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class UserResponse {
+
+    private String username;
+    private String email;
+    private String nickname;
+    private UserRole role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .email(user.getEmail())
+                .role(user.getUserRole())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
+}
