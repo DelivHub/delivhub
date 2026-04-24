866a21902f107d1e6658089d858b514fcf4b6e4b
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2026-04-24 12:03:58 +0900

    fix: [KMW] Store 유저 인증 서비스 적용

--- src/main/java/com/sparta/delivhub/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/delivhub/domain/store/controller/StoreController.java
@@ -6,8 +6,12 @@ import com.sparta.delivhub.domain.store.dto.response.StoreDetailResponseDto;
 import com.sparta.delivhub.domain.store.dto.response.StoreIdResponseDto;
 import com.sparta.delivhub.domain.store.dto.response.StoreListResponseDto;
 import com.sparta.delivhub.domain.store.service.StoreService;
+import com.sparta.delivhub.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,14 +24,15 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping("/stores")
-    public ApiResponse<StoreIdResponseDto> createStore(@RequestBody StoreRequestDto storeRequestDto) {
-        StoreIdResponseDto id = storeService.createStore(storeRequestDto);
+    public ApiResponse<StoreIdResponseDto> createStore(@RequestBody StoreRequestDto storeRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        StoreIdResponseDto id = storeService.createStore(storeRequestDto, userDetails.getUsername());
         return ApiResponse.success(id);
     }
 
     @GetMapping("/stores")
-    public ApiResponse<List<StoreListResponseDto>> getAllStores() {
-        return ApiResponse.success(storeService.findAllStores());
+    public ApiResponse<List<StoreListResponseDto>> getAllStores(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "createdAt,DESC") String sort) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
+        return ApiResponse.success(storeService.findAllStores(pageable));
     }
 
     @GetMapping("/stores/{storeId}")
@@ -36,14 +41,14 @@ public class StoreController {
     }
 
     @PutMapping("/stores/{storeId}")
-    public ApiResponse<StoreIdResponseDto> updateStore(@PathVariable("storeId") UUID storeId, @RequestBody StoreRequestDto storeRequestDto) {
-        StoreIdResponseDto id = storeService.updateStore(storeId, storeRequestDto);
+    public ApiResponse<StoreIdResponseDto> updateStore(@PathVariable("storeId") UUID storeId, @RequestBody StoreRequestDto storeRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        StoreIdResponseDto id = storeService.updateStore(storeId, storeRequestDto, userDetails.getUsername());
         return ApiResponse.success(id);
     }
 
     @DeleteMapping("/stores/{storeId}")
-    public ApiResponse<StoreIdResponseDto> deleteStore(@PathVariable("storeId") UUID storeId) {
-        StoreIdResponseDto id = storeService.deleteStore(storeId, "ADMIN");
+    public ApiResponse<StoreIdResponseDto> deleteStore(@PathVariable("storeId") UUID storeId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        StoreIdResponseDto id = storeService.deleteStore(storeId, userDetails.getUsername());
         return ApiResponse.success(id);
     }
 
--- src/main/java/com/sparta/delivhub/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/delivhub/domain/store/service/StoreService.java
@@ -2,6 +2,7 @@ package com.sparta.delivhub.domain.store.service;
 
 import com.sparta.delivhub.common.dto.BusinessException;
 import com.sparta.delivhub.common.dto.ErrorCode;
+import com.sparta.delivhub.common.util.AuthorizationUtils;
 import com.sparta.delivhub.domain.area.entity.Area;
 import com.sparta.delivhub.domain.area.repository.AreaRepository;
 import com.sparta.delivhub.domain.category.entity.Category;
@@ -12,9 +13,14 @@ import com.sparta.delivhub.domain.store.dto.response.StoreIdResponseDto;
 import com.sparta.delivhub.domain.store.dto.response.StoreListResponseDto;
 import com.sparta.delivhub.domain.store.entity.Store;
 import com.sparta.delivhub.domain.store.repository.StoreRepository;
+import com.sparta.delivhub.domain.user.entity.User;
+import com.sparta.delivhub.domain.user.entity.UserRole;
+import com.sparta.delivhub.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,9 +32,17 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final CategoryRepository categoryRepository;
     private final AreaRepository areaRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public StoreIdResponseDto createStore(StoreRequestDto storeRequestDto) {
+    public StoreIdResponseDto createStore(StoreRequestDto storeRequestDto, String userId) {
+        User user = userRepository.findByUsernameAndDeletedAtIsNull(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        if (user.getUserRole() != UserRole.OWNER) {
+            throw new BusinessException(ErrorCode.ACCESS_DENIED);
+        }
+
         Category category = categoryRepository.findById(storeRequestDto.getCategoryId())
                 .orElseThrow(() -> new BusinessException(ErrorCode.CATEGORY_NOT_FOUND));
 
@@ -43,6 +57,7 @@ public class StoreService {
                 .number(storeRequestDto.getNumber())
                 .isHidden(storeRequestDto.getIsHidden())
                 .build();
+
         storeRepository.save(store);
 
 
@@ -51,8 +66,8 @@ public class StoreService {
                 .build();
     }
 
-    public List<StoreListResponseDto> findAllStores() {
-        return storeRepository.findAll().stream()
+    public List<StoreListResponseDto> findAllStores(Pageable pageable) {
+        return storeRepository.findAll(pageable).stream()
                 .map(StoreListResponseDto::new)
                 .toList();
     }
@@ -62,14 +77,20 @@ public class StoreService {
                 .map(StoreDetailResponseDto::new)
                 .orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
     }
+
     @Transactional
-    public StoreIdResponseDto updateStore(UUID id, StoreRequestDto storeRequestDto) {
+    public StoreIdResponseDto updateStore(UUID id, StoreRequestDto storeRequestDto, String userId) {
         Store store = storeRepository.findById(id)
                 .orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
         Category category = categoryRepository.findById(storeRequestDto.getCategoryId())
                 .orElseThrow(() -> new BusinessException(ErrorCode.CATEGORY_NOT_FOUND));
         Area area = areaRepository.findById(storeRequestDto.getAreaId())
                 .orElseThrow(() -> new BusinessException(ErrorCode.AREA_NOT_FOUND_ON_READ));
+
+        User user = userRepository.findByUsernameAndDeletedAtIsNull(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        AuthorizationUtils.checkOwnerOrAdminPermission(user, store.getOwner().getUsername());
 
         store.updateStore(
                 storeRequestDto.getName(),
@@ -89,6 +110,10 @@ public class StoreService {
     public StoreIdResponseDto deleteStore(UUID id, String userId) {
         Store store = storeRepository.findById(id)
                 .orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
+        User user = userRepository.findByUsernameAndDeletedAtIsNull(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        AuthorizationUtils.checkOwnerOrAdminPermission(user, store.getOwner().getUsername());
 
         store.softDelete(userId);
 
